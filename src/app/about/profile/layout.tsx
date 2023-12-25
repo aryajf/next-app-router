f22548418c13c355802ec204dd1944ab106a88eb
AryaJF <aryajavasfatih888@gmail.com>
2023-12-25 22:17:22 +0700

    first commit

---
+++ src/app/about/profile/layout.tsx
@@ -0,0 +1,12 @@
+export default function ProfileLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <h1>Hello Semua</h1>
+      {children}
+    </>
+  );
+}
---
+++ src/app/navbar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+
+export default function Navbar() {
+  const pathname = usePathname();
+  const router = useRouter();
+  return (
+    <nav className="flex bg-gray-800 py-2 px-5 justify-between">
+      <div className="flex">
+        <h1 className="text-white">Navbar</h1>
+        <ul className="flex ml-5">
+          <Link href="/">
+            <li className={`mr-3 ${pathname == '/' ? 'text-blue-300' : 'text-white'}`}>Home</li>
+          </Link>
+          <Link href="/about">
+            <li className={`mr-3 ${pathname == '/about' ? 'text-blue-300' : 'text-white'}`}>About</li>
+          </Link>
+          <Link href="/about/profile">
+            <li className={`mr-3 ${pathname == '/about/profile' ? 'text-blue-300' : 'text-white'}`}>Profile</li>
+          </Link>
+        </ul>
+      </div>
+      <div>
+        <button onClick={() => router.push('/login')} className="bg-white rounded-md px-3 h-7">Login</button>
+      </div>
+    </nav>
+  );
+}
